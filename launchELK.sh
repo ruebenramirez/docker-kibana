2f0420fffda0dd285f7ba3b5b0d65b3940c5a524
Rueben Ramirez <ruebenramirez@gmail.com>
2014-12-11 18:28:45 +0000

    reworked kibana4beta2 container on base debian

---
+++ launchELK.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+docker rm -f elasticsearch
+docker rm -f kibana4
+
+docker pull dockerfile/elasticsearch:latest
+docker rmi kibana4beta2
+docker build -t kibana4beta2 .
+
+
+docker run -d \
+    -p 9200:9200 \
+    -p 9300:9300 \
+    --name elasticsearch \
+    dockerfile/elasticsearch
+
+docker run -d \
+    -p 82:80 \
+    -e KIBANA_SECURE=false \
+    --link elasticsearch:es \
+    --name kibana4 \
+    kibana4beta2
